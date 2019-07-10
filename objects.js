9417cd09fb4b0b98b53dbec86759e73785896574
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-10 22:37:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = 'songTitle'
 }
 
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+}
